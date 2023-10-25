ce114f6d2acd7aee43461fc5df69b3e50871cd59
ButterflyDew <1295407399@qq.com>
2023-10-25 11:27:21 +0800

    CBA_v_0.999

---
+++ debug_cba.cpp
@@ -0,0 +1,67 @@
+#include "CBA.h"
+#include "Hub-Labeling/global.h"
+Graph G;
+CBA cba;
+vector<int> extractIntegers(const string& input) 
+{
+    vector<int> integers;
+    istringstream iss(input);
+    string token;
+
+    while (iss >> token) 
+    {
+        try 
+        {
+            int number = stoi(token);
+            integers.push_back(number);
+        } catch (const invalid_argument&) 
+        {
+            cerr << "not a number" << endl;
+        }
+    }
+
+    return integers;
+}
+
+void readQ(vector <vector <int> > &Query)
+{
+    string filename = "Data/Query.in";
+
+    ifstream inputFile(filename);
+
+    if (!inputFile.is_open()) 
+        cerr << "Can't open file: " << filename << endl;
+
+    string line;
+    getline(inputFile, line);
+
+    int n = extractIntegers(line)[0];
+
+    while (getline(inputFile, line)) 
+    {
+        //cout << line << endl;
+        auto num = extractIntegers(line);
+        Query.push_back(num);
+    }
+    inputFile.close();
+}
+void check_cba()
+{
+    // int n = 4, m = 5, M = 3;
+    // G.random_graph_nm(n, m, M);
+    G.read();
+    int n = G.n;
+    int D = generateRandomNumber(1, n/2);
+
+    vector <vector <int> > Query;
+    readQ(Query);
+
+    Tree T = cba.Go_CBA(G, Query, D);
+    T.Print();
+}
+
+int main()
+{
+    check_cba();
+    return 0;
+}
